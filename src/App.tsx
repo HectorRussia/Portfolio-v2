c334024e0485fa74acaca05329461ebcc3598c87
Ponkrit <ponkrit.wo@hotmail.com>
2025-12-03 11:05:06 +0700

    initialize project with ฺBun React, TypeScript, and Vite setup; add essential configuration files and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { FaGolang } from "react-icons/fa6";
+function App() {
+
+  return (
+    <>
+      <div className='bg-slate-800 h-screen'>  <FaGolang size={50} color='blue'/></div>
+    
+    </>
+  )
+}
+
+export default App
